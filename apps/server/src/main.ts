b50c84335a57220f6da697cb9be78ce83f68b482
roee1454 <roee1454@gmail.com>
2026-01-16 23:59:33 +0200

    Added safe origins for app root

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -9,8 +9,12 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, { rawBody: true });
 
+  const origins = [process.env.CLIENT_ORIGIN, 'http://localhost:4200'].filter(
+    Boolean,
+  ) as string[];
+
   app.enableCors({
-    origin: [process.env.CLIENT_ORIGIN as string, 'http://localhost:4200'],
+    origin: origins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
